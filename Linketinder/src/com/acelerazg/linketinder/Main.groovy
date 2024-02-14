cd3a4e995a00f99ef96f753e8b32c8ffdd5270f2
Geovane de Andrade <geovanedeandradegrangeiro@gmail.com>
2024-02-14 14:44:37 -0300

    Feat: menu simples via terminal adicionado

--- Linketinder/src/com/acelerazg/linketinder/Main.groovy
+++ Linketinder/src/com/acelerazg/linketinder/Main.groovy
@@ -1,2 +1,46 @@
 package com.acelerazg.linketinder
 
+import com.acelerazg.linketinder.model.impl.PessoaFisica
+import com.acelerazg.linketinder.model.impl.PessoaJuridica
+
+PessoaFisica pessoaFisica = new PessoaFisica()
+PessoaJuridica pessoaJuridica = new PessoaJuridica()
+
+println("---------------------------------")
+println("Seja bem vindo! Gostaria de reproduzir o Linketinder? [S/N]");
+
+String reproduz = System.in.newReader().readLine()
+
+
+while (reproduz.equalsIgnoreCase("S")) {
+    System.out.println("----------------------------------------------");
+    System.out.println("Seja bem vindo! Digite o número de acordo com o que deseja: \n" +
+            "[1] Listar Candidatos\n" +
+            "[2] Listar Empresas\n" +
+            "[3] Encerrar programa."
+    );
+
+    Integer resposta = System.in.newReader().readLine() as Integer
+    println("----------------------------------------------");
+
+    switch (resposta) {
+        case 1: {
+            pessoaFisica.listarPessoas()
+            break
+        }
+        case 2: {
+            pessoaJuridica.listarPessoas()
+            break
+        }
+        case 3: {
+            reproduz = "n"
+            System.out.println("Encerrando...")
+            break
+        }
+        default: {
+            System.out.println("Digite um número válido.")
+        }
+    }
+}
+if (reproduz.equalsIgnoreCase("n")) System.out.println("Até a próxima!")
+
