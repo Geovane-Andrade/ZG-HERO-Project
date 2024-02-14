3f4afa367f65519bde3243068209ee3ebc79f001
Geovane de Andrade <geovanedeandradegrangeiro@gmail.com>
2024-02-14 14:46:17 -0300

    Feat: método listarPessoa e construtor vazio adicionado

--- Linketinder/src/com/acelerazg/linketinder/model/impl/Pessoa.groovy
+++ Linketinder/src/com/acelerazg/linketinder/model/impl/Pessoa.groovy
@@ -3,4 +3,33 @@ package com.acelerazg.linketinder.model.impl
 import com.acelerazg.linketinder.model.PessoaInterface
 
 class Pessoa implements PessoaInterface{
+
+    String nome
+    String email
+    String identificador
+    String estado
+    String cep
+    String descricao
+    def competencias = new ArrayList(5)
+
+    Pessoa() {
+    }
+
+    Pessoa(String nome, String email, String identificador, String estado,
+           String cep, String descricao, ArrayList competencias) {
+        this.nome = nome
+        this.email = email
+        this.identificador = identificador
+        this.estado = estado
+        this.cep = cep
+        this.descricao = descricao
+        this.competencias = competencias
+    }
+
+    @Override
+    void listarPessoas() {
+
+    }
+
+
 }
